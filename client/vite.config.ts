164666d631d70277c890079145990d4b9f4074be
KayolEnjoy <127608164+KayolEnjoy@users.noreply.github.com>
2026-03-05 17:07:01 +0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -88,11 +88,11 @@ export default defineConfig(({ command }) => ({
       },
       includeAssets: [],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'Trico',
+        short_name: 'Trico',
         display: 'standalone',
         background_color: '#000000',
-        theme_color: '#009688',
+        theme_color: '#4F46E5',
         icons: [
           {
             src: 'assets/favicon-32x32.png',
